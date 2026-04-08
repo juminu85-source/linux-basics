30b8517cde3fa3616036101a52599d2cea3dce00
ubuntu <juminu85@gmail.com>
2026-04-08 14:01:26 +0900

    temp fix

--- git-auto.sh
+++ git-auto.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# 1. 깃허브에서 최신 내용 가져오기
-git pull https://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@github.com/juminu85-source/linux-basics.git main --rebase
+# 1. 깃허브 비밀번호(토큰)를 한 번만 입력하면 기억하게 설정
+git config --global credential.helper store
 
-# 2. 변경사항 담기
+# 2. 최신 내용 가져와서 합치기 (오류 방지)
+git pull origin main --rebase
+
+# 3. 변경사항 담기
 git add .
 
-# 3. 커밋 메시지 입력받기
+# 4. 커밋 메시지 입력받기
 echo -n "커밋 메시지 입력 : "
 read commitmsg
 
-# 4. 깃허브로 강제 전송 (토큰 주소 사용)
+# 5. 깃허브로 올리기
 git commit -m "$commitmsg"
-git push https://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@github.com/juminu85-source/linux-basics.git main --force
+git push origin main --force
