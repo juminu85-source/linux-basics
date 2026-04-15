adbdacb811ad74762d3933aa625688597b6ebfe8
juminu85-source <juminu85@gmail.com>
2026-04-15 14:15:24 +0900

    Delete basic4 directory

--- basic4/quiz4-1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-touch test1.txt
-chmod 644 test1.txt
-ls -l test1.txt
--- basic4/quiz4-5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-touch multi_test.txt
-sudo chown ubuntu2:ubuntu3 multi_test.txt
-ls -l multi_test.txt
